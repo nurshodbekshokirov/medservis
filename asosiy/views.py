b1249e2300e0776b2ab0370416d2e608dcd25d3f
Nurshodbek <nurshodbekshokirov@gmail.com>
2024-03-25 21:09:10 -0700

    register togrilandi

--- asosiy/views.py
+++ asosiy/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate, login,logout
@@ -34,32 +35,26 @@ class MIjozMODELVIEW(ModelViewSet):
 
 
 class RegisterAPIView(APIView):
-    # Agar siz APIView'da autentifikatsiya va ruxsatlarni ham tekshirmoqchi bo'lsangiz,
-    # quyidagi satrlarni qo'shishingiz mumkin. Lekin asosan tekshiruvni `post` metodida amalga oshiramiz.
+
     permission_classes = [IsAdminUser]  # Faqat admin foydalanuvchilarga ruxsat berish
+
+    @swagger_auto_schema(request_body=MIJOZSERIALIZERS)
+    # authentication_classes = [JWTAuthentication]
 
     def post(self, request, *args, **kwargs):
         # Foydalanuvchi superuser ekanligini tekshirish
         if not request.user.is_superuser:
-            return Response({'xabar': 'Faqat superuserlar ro\'yxatdan o\'ta oladi.'}, status=status.HTTP_403_FORBIDDEN)
+            return Response({'xabar': "Faqat superuserlar ro'yxatdan o'ta oladi."}, status=status.HTTP_403_FORBIDDEN)
 
         serializer = MIJOZSERIALIZERS(data=request.data)
         if serializer.is_valid():
-            mijoz = serializer.save()
-            # Foydalanuvchi uchun JWT tokenlarini yaratish
-            refresh = RefreshToken.for_user(mijoz.user)
-            tokenlar = {
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            }
-            # Mijoz ma'lumotlari va tokenlarni qaytarish
-            data = {
-                'mijoz': MIJOZSERIALIZERS(mijoz).data,
-                'tokenlar': tokenlar,
-            }
-            return Response(data, status=status.HTTP_201_CREATED)
+            serializer.save()
+            # Mijoz ma'lumotlarini qaytarish (tokenlarsiz)
+            return Response({'xabar': "Mijoz muvaffaqiyatli ro'yxatdan o'tdi."}, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class LoginAPIView(APIView):
     def post(self, request):
         username = request.data.get('username')
